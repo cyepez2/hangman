e334c8f2e0503c9797176c5557a188efef06743f
Camelot Yepez <113930338+cyepez2@users.noreply.github.com>
2022-10-27 17:53:10 +0000

    learning functions

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 #from methods_help import method_help
-from functions import welcome, add_numbers, num_dividedFrom_add_numbers, greet, welcome
-# from returnStuff import returnS
+#from functions import welcome, add_numbers, num_dividedFrom_add_numbers, greet, welcome
+from returnStuff import power
 
 #method_help
 # pass - make a function and want to do something, but not sure what
 
 #function()
 #yari = welcome("Yahaira")
-added = add_numbers(4,5)
-print(added)
-
-final = num_dividedFrom_add_numbers(45, added)
-print(final)
+#added = add_numbers(4,5)
+#print(added)
 #returnS()
+#final = num_dividedFrom_add_numbers(45, added)
+#print(final)
+#returnS()
+power2 = power(3,7)
+print(power2)
+#greet()
 
-greet()
-
-welcome()
+#()
