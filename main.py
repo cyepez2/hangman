a210cdfa9cc0e7c6731e6c46fce5c583e7e3db51
Camelot Yepez <113930338+cyepez2@users.noreply.github.com>
2022-10-27 17:28:12 +0000

    learning variables

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from methods_help import method_help
-# from functions import function
+#from methods_help import method_help
+from functions import welcome, add_numbers, num_dividedFrom_add_numbers, greet
 # from returnStuff import returnS
 
-method_help()
+#method_help
 # pass - make a function and want to do something, but not sure what
 
 #function()
+yari = welcome("Yahaira")
+added = add_numbers(4,5)
+print(added)
+
+final = num_dividedFrom_add_numbers(45, added)
+print(final)
 #returnS()
