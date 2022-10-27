9304be9ca7e2f9876a3b96501b90ab1460910799
Camelot Yepez <113930338+cyepez2@users.noreply.github.com>
2022-10-27 16:55:55 +0000

    learned the lstrip() method learned the insert() method learned the isdisjoint() method

---
+++ main.py
@@ -0,0 +1,9 @@
+from methods_help import method_help
+# from functions import function
+# from returnStuff import returnS
+
+method_help()
+# pass - make a function and want to do something, but not sure what
+
+#function()
+#returnS()
